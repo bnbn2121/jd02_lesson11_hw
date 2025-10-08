24230f9ca182823688d528a5472167cb585d1f15
Dmitry Sheluto <shelutods@gmail.com>
2025-10-08 11:03:45 +0300

    add passwordHasher in repo

---
+++ src/main/java/com/edu/less08/service/util/PasswordHasher.java
@@ -0,0 +1,16 @@
+package com.edu.less08.service.util;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordHasher {
+    public String hashPassword(String password) {
+        if (password == null) {
+            throw new IllegalArgumentException();
+        }
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public boolean checkPassword(String passwordForCheck, String hashedPassword) {
+        return BCrypt.checkpw(passwordForCheck, hashedPassword);
+    }
+}
